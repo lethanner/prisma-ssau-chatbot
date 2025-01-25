ff926f833c7d08723a7517655789b1b571760e59
lethanner <50079644+lethanner@users.noreply.github.com>
2025-01-26 00:58:56 +0400

    правки

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+# я плохо пишу на python, простите
+
 from requests import get as httpget
 import team
+import logging
 import json
 import configparser
 import vk
@@ -7,6 +10,13 @@ import vk
 configfile = 'prisma.ini'
 config = configparser.ConfigParser()
 config.read(configfile)
+
+# логирование оставляю для отлова ошибок в случае их возникновения
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+loghandler = logging.FileHandler('prismabot.log', 'a', 'utf-8')
+loghandler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
+logger.addHandler(loghandler)
 
 vk = vk.API(access_token=config['auth']['group'],v=5.199)
 group_id = config.getint('misc','group_id')
@@ -41,8 +51,11 @@ def sendMessage(peer_id: int, text: str, keyboard: str = '{"buttons": []}'):
     try:
         vk.messages.send(peer_id=peer_id,message=text,random_id=0,
                          keyboard=keyboard)
+        print("[BOT] to id{0}: {1}".format(peer_id, text))
+        logger.info("To id%i: %s, keyboard: %s", peer_id, text, keyboard)
     except Exception as e:
         print("[ERROR] Failed to send message to", peer_id, "!", e)
+        logger.error("Can't send message to %i", peer_id)
         pass
 
 def takeMessage(events):
@@ -62,14 +75,20 @@ def takeMessage(events):
         text = obj.get('text', "")
         from_id = obj['peer_id']
         print("[MESSAGE] id{0}: {1}; payload: {2}".format(from_id, text, payload))
+        logger.info("From id%i: %s, payload: %s", from_id, text, payload)
 
         command = payload.get('command', "")
         data = int(payload.get('data', 0))
 
         # команда "начать" или соответствующая кнопка (приветственное сообщение)
         if command == "start" or text.lower() == "начать":
-            sendMessage(from_id, team.greeting, dirs_keyboard)
-            usercache.pop(from_id, None) # удалить юзера из кэша (на случай, если он начнёт заново)
+            director = next(((key, value) for key, value in team.directorate.items() if value[0] == from_id), None)
+            # если кнопку нажал замыка, то ему просто вываливается инфа о нём
+            if (director):
+                sendMessage(from_id, "{0} - {1}".format(director[1][1], director[0]))
+            else: # остальные получат предложение начать выбирать направление
+                sendMessage(from_id, team.greeting, dirs_keyboard)
+                usercache.pop(from_id, None) # удалить юзера из кэша (на случай, если он начнёт заново)
         # нажатие кнопки с названием направления (выброс описания с кнопкой "хочу сюда")
         elif command == "descr" and 0 <= data < team.dir_count:
             keyboard = json.dumps({
@@ -83,6 +102,8 @@ def takeMessage(events):
                     "color": "positive"
                 }]]})
             sendMessage(from_id, team.dir_description(data), keyboard)
+        elif text == "/lol" and from_id == config.getint('auth','bot_admin'):
+            a = 1/0
         # нажатие кнопки "хочу сюда"
         elif command == "reg":
             user = vk.users.get(user_ids=from_id,fields='is_verified',lang=0)
@@ -139,9 +160,16 @@ def takeMessage(events):
                     from_id, usercache[from_id][0],
                     team.directions[direction_sel]['name'],
                     text)
-                sendMessage(team.get_director(direction_sel)[0], msg)
+                dir_director = team.get_director(direction_sel)[0]
+                if dir_director != 0 and dir_director != team.directorate['owner'][0]:
+                    sendMessage(dir_director, msg)
+                sendMessage(team.directorate['owner'][0], msg)
+                sendMessage(team.directorate['admin'][0], msg)
                 # т.к. это последняя стадия регистрации, отправляем сообщение "Спасибо" и удаляем юзера из кэша
-                sendMessage(from_id, team.thanks)
+                # дополнительно сохраняем инфу в файл, на случай если замыки случайно удалят сообщения
+                sendMessage(from_id, team.thanks.format(config['misc']['chat_url']))
+                with open("database/{0}.txt".format(team.directions[direction_sel]['codename']), 'a+', encoding='utf-8') as file:
+                    file.write("{0}\r\nhttps://vk.com/id{1}\r\n\r\n".format(usercache[from_id][0], from_id))
                 del usercache[from_id]
         
 # главный цЫкл бота
@@ -152,6 +180,7 @@ try:
 
     ts = config.getint('longpoll', 'ts')
     print('[SYSTEM] Bot is started.')
+    logger.info("Bot is started.")
     while True:
         events = httpget(config['longpoll']['server'], params={'act': 'a_check',
                                                                'key': config['longpoll']['key'],
@@ -160,11 +189,18 @@ try:
         if error in (2, 3):
             updateLongPoll()
         else:
-            ts = events.json()['ts']
-            #print(events.json())
-            takeMessage(events.json())
+            try:
+                ts = events.json()['ts']
+                #print(events.json())
+                takeMessage(events.json())
+            except Exception as e:
+                print("[ERROR] Something went wrong!!!", e)
+                sendMessage(config.getint('auth','bot_admin'), "боту плохо, посмотри логи... " + str(e))
+                logger.error(e, exc_info=1)
+                pass
         
 except KeyboardInterrupt:
     saveConfig()
     print('Exiting.')
+    logger.info("Stopping.")
     exit(0)
